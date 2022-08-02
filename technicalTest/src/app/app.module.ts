3ebd828ba2f3e9c82c482493fda5440ac0bf0a1c
Natalia <harumy08@gmail.com>
2022-08-01 21:52:44 -0500

    Se crean los componentes que se van a usar

--- technicalTest/src/app/app.module.ts
+++ technicalTest/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    FooterComponent,
+    NavbarComponent,
+    TableListComponent,
+    WelcomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
